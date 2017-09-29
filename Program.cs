18133d2f3aa1005d9df9bd709e8372638a97415c
Matias Dieguez <matiasdieguez@hotmail.com>
2017-09-29 10:26:34 -0300

    agregado de archivos faltantes

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ArgenSearch;
+
+namespace robotargensearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 31000000; i < 31000010; i++)
+            {
+                Console.WriteLine("DNI: " + i);
+
+                var persona = ArgenSearchClient.Search(i).Result;
+
+                if (persona.Data == null)
+                    return;
+
+                var cuil = persona.Data[0];
+
+                var datos = ArgenSearchClient.Detail(cuil).Result;
+
+                if (datos.Data.DomicilioFiscal == null)
+                    Console.WriteLine(datos.Data.Nombre);
+                else
+                    Console.WriteLine(datos.Data.Nombre + " " + datos.Data.DomicilioFiscal.Direccion);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
